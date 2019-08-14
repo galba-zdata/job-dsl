bc41006fce17971e60eed179575f301e39afed9f
galba-zdata <53880446+galba-zdata@users.noreply.github.com>
2019-08-14 17:20:38 -0500

    Update galba-zdata.groovy

--- galba-zdata.groovy
+++ galba-zdata.groovy
@@ -1,7 +1,16 @@
-organizationFolder('galba-zdata') {
-    description('This contains branch source jobs for Bitbucket and GitHub')
-    displayName('galba-zdatar')
+  organizationFolder('example-org') {
+    description('This contains branch source jobs for example-org GitHub')
+    displayName('example-org')
     triggers {
-        periodic(86400)
+      periodic(2400)
     }
-}
+    organizations {
+      github {
+        repoOwner("galba-zdata")
+        apiUri("https://api.github.com")
+        traits {
+          publicRepoPullRequestFilterTrait()
+        }
+      }
+    }
+  }
